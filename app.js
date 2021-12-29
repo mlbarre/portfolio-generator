f5f81531d56aaf101fb3c801683fdd6b5dc79fc0
mlbarre <mlbarrera1538@gmail.com>
2021-12-28 23:20:36 -0600

    added html structure to js file

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const [name, github] = profileDataArgs;
 
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err)  throw new Error(err);
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+// console.log(name, github); (TEST)
+// console.log(generatePage(name, github));
+// console.log(generatePage());
+// console.log(profileDataArgs); (12.28.2021)
+// const printProfileData = (profileDataArr) => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
